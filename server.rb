be0929743e324559a68035656a32b68021a417ee
anniezyao <90427620+anniezyao@users.noreply.github.com>
2024-10-15 19:45:41 -0700

    Update server.rb

--- server.rb
+++ server.rb
@@ -8,3 +8,35 @@ CLIENT_SECRET = ENV['3d43cf03b7cd0868e4cf9692015ad05ad41fbfef']
 get '/' do
   erb :index, :locals => {:client_id => CLIENT_ID}
 end
+
+get '/callback' do
+  # get temporary GitHub code...
+  session_code = request.env['rack.request.query_hash']['code']
+
+  # ... and POST it back to GitHub
+  result = RestClient.post('https://github.com/login/oauth/access_token',
+                          {:client_id => CLIENT_ID,
+                           :client_secret => CLIENT_SECRET,
+                           :code => session_code},
+                           :accept => :json)
+
+  # extract the token and granted scopes
+  access_token = JSON.parse(result)['access_token']
+
+  # check if we were granted user:email scope
+  scopes = JSON.parse(result)['scope'].split(',')
+  has_user_email_scope = scopes.include? 'user:email' || scopes.include? 'user'
+end
+
+# fetch user information
+auth_result = JSON.parse(RestClient.get('https://api.github.com/user',
+                                        {:params => {:access_token => access_token}}))
+
+# if the user authorized it, fetch private emails
+if has_user_email_scope
+  auth_result['private_emails'] =
+    JSON.parse(RestClient.get('https://api.github.com/user/emails',
+                              {:params => {:access_token => access_token}}))
+end
+
+erb :basic, :locals => auth_result
